fe3edf43f3b44809cb3dc69742f5b6afec5c9f3e
evaaaaawu <yiju.wu1@gmail.com>
2024-01-18 19:28:27 +0800

    feat: add update and delete api

--- src/api/todos.js
+++ src/api/todos.js
@@ -24,5 +24,25 @@ export const createTodo = async (payload) => {
   }
 };
 
-export const patchTodo = () => {};
-export const deleteTodo = () => {};
+export const patchTodo = async (payload) => {
+  const { id, title, isDone } = payload;
+  try {
+    const res = await axios.patch(`${baseUrl}/todos/${id}`, {
+      title,
+      isDone,
+    });
+
+    return res.data;
+  } catch (error) {
+    console.error('[Patch Todo failed]:', error);
+  }
+};
+
+export const deleteTodo = async (id) => {
+  try {
+    const res = await axios.delete(`${baseUrl}/todos/${id}`);
+    return res.data;
+  } catch (error) {
+    console.error('[Delete Todo failed]:', error);
+  }
+};
